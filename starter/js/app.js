090bb2a08f06b18f6e480887b061b92a425e5844
theodoros <tsphikas@gmail.com>
2022-03-31 23:54:38 +0100

    Landing Page Complete

--- starter/js/app.js
+++ starter/js/app.js
@@ -3,7 +3,7 @@ const menuLinks = document.querySelector(".navbar_menu");
 
 menu.addEventListener("click", function () {
   /* Nav Bar Animation */
-  //menu.classList.toggle("is-active");
+  menu.classList.toggle("is-active");
 
   /* Nav bar scoll down */
   menuLinks.classList.toggle("active");
--- starter/js/preloader.js
+++ starter/js/preloader.js
@@ -10,7 +10,11 @@ const loadWait = function () {
   setTimeout(function () {
     loader.style.display = "none";
     enableScroll();
-  }, 0); /* 5000 */
+    console.log("3 sec");
+    anim();
+  }, 3000);
+
+  /* 5000 */
 };
 
 //Function Disable Scroll on Desktops
@@ -33,3 +37,37 @@ function initialiser() {
 }
 
 initialiser();
+
+/* Animation Delay */
+//Animations Elements
+const introLine1 = document.querySelector(".intro_content_h3");
+const introLine2 = document.querySelector(".intro_content_h2");
+const introLine3 = document.querySelector(".intro_content_build_h2");
+const introLine4 = document.querySelector(".intro_content_p");
+const myWorkBtn = document.querySelector(".my_work_btn");
+const navBar = document.querySelector(".navbar_menu");
+const navLogo = document.getElementById("navbar_logo");
+
+//Anim Function
+const anim = function () {
+  introLine1.style.animation = "moveInRight ease-in-out 1.5s 1 normal";
+  introLine1.style.animationFillMode = "forwards";
+
+  introLine2.style.animation = "moveInRight ease-in-out 1.5s 1 0.5s normal";
+  introLine2.style.animationFillMode = "forwards";
+
+  introLine3.style.animation = "moveInRight ease-in-out 1.5s 1 1s normal";
+  introLine3.style.animationFillMode = "forwards";
+
+  introLine4.style.animation = "moveInRight ease-in-out 1.5s 1 1.5s normal";
+  introLine4.style.animationFillMode = "forwards";
+
+  myWorkBtn.style.animation = "fadeIn ease-in-out 1.5s 1 1.5s normal";
+  myWorkBtn.style.animationFillMode = "forwards";
+
+  navBar.style.animation = "moveInDown ease-in-out 1.5s 1 0.5s normal";
+  navBar.style.animationFillMode = "forwards";
+
+  navLogo.style.animation = "moveInDown ease-in-out 1.5s 1 0.5s normal";
+  navLogo.style.animationFillMode = "forwards";
+};
